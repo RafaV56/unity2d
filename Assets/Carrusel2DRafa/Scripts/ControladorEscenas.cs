cf7f958ee05577fea98012544668f5b18373ce8b
LOLO\Pc <rafael_v56@hotmail.com>
2017-09-28 15:19:39 +0200

    agregado funcionalidad a botones jugar y salir

---
+++ Assets/Carrusel2DRafa/Scripts/ControladorEscenas.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ControladorEscenas : MonoBehaviour {
+
+	public void CambiarEscena(string nombre)
+    {
+        //Cambiar de escena de en el boton jugar de portada
+        SceneManager.LoadScene(nombre);
+
+    }
+
+
+    public void SalirJuego()
+    {
+        //Método para salir del juego
+        Application.Quit();
+
+    }
+}
