c94b3e0d46e9a372a9c42085e20efcf4b137424d
LOLO\Pc <rafael_v56@hotmail.com>
2017-09-29 22:44:18 +0200

    Agregado la nave y su movimiento en el script

---
+++ Assets/Sprites/Scripts/ControladorNave.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControladorNave : MonoBehaviour {
+
+    public float speed = 5f;
+    public float marco = 1f;
+	
+	// Actualiza cada fotograma
+	void Update() {
+
+        //movimiento con variación de horizontal 
+
+        float entradaHorizontal = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(entradaHorizontal * speed * Time.deltaTime, 0, 0);
+
+        //movimiento con variación de velocidad 
+
+        float entradaVertical = Input.GetAxis("Vertical");
+        transform.position += new Vector3(0, entradaVertical * speed * Time.deltaTime, 0);
+
+
+        //clamping rectificación de coordenadas
+        float nuevaX = Mathf.Clamp(transform.position.x, -10+marco, 10-marco);
+        float nuevaY = Mathf.Clamp(transform.position.y, -10+marco, 10-marco);
+        transform.position = new Vector3(nuevaX, nuevaY, 0);
+    }
+}
