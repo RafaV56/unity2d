f16f3274fd248bd3c693c40183212312b45cad6a
LOLO\Pc <rafael_v56@hotmail.com>
2017-09-29 21:33:04 +0200

    Agreago escena para controlar colosiones en consola, y trigers

---
+++ Assets/FísicasYColisiones/ScriptsFisica/Plataforma.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Plataforma : MonoBehaviour {
+
+    //eventos de colisiones
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Ha ocurrido un evento Colisión");
+    }
+
+    //Eventos de triger
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Ha ocurrido un evento de triger");
+    }
+}
