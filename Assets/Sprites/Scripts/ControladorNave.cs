f813045e1395fe78731299620295ccd9148bc38f
LOLO\Pc <rafael_v56@hotmail.com>
2017-09-29 23:30:50 +0200

    Se mueve la nave y dispara en rafa con la tecla espacio.

--- Assets/Sprites/Scripts/ControladorNave.cs
+++ Assets/Sprites/Scripts/ControladorNave.cs
@@ -3,8 +3,14 @@ using System.Collections;
 
 public class ControladorNave : MonoBehaviour {
 
+    //velocidad de la nave
     public float speed = 5f;
+    //marco del juego
     public float marco = 1f;
+    //Balas
+    public GameObject balas;
+
+
 	
 	// Actualiza cada fotograma
 	void Update() {
@@ -24,5 +30,28 @@ public class ControladorNave : MonoBehaviour {
         float nuevaX = Mathf.Clamp(transform.position.x, -10+marco, 10-marco);
         float nuevaY = Mathf.Clamp(transform.position.y, -10+marco, 10-marco);
         transform.position = new Vector3(nuevaX, nuevaY, 0);
+
+        //Disparo
+        if (Input.GetKeyDown(KeyCode.Space)){
+            //poner el repetidor en marcha de rafaga. 
+            InvokeRepeating("Fuego", 0.001f, 0.25f);
+        }else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            //parar la rafagas
+            CancelInvoke("Fuego");
+        }
     }
+
+    void Fuego()
+    {
+        var nave = GameObject.Find("Nave");
+        if (nave != null)
+        {
+            Vector3 posIzq = nave.transform.position + Vector3.up * 1.3f + Vector3.left * 0.65f;
+            Vector3 posDer = nave.transform.position + Vector3.up * 1.3f + Vector3.right * 0.65f;
+            Instantiate(balas, posIzq, Quaternion.identity);
+            Instantiate(balas, posDer, Quaternion.identity);
+        }
+    }
+
 }
---
+++ Assets/Sprites/Scripts/balaControlador.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class balaControlador : MonoBehaviour {
+
+    public float velocidadBala = 8f;
+
+	// Use this for initialization
+	void Start () {
+        //movemos la bala dinamicamente
+        this.GetComponent<Rigidbody2D>().velocity = new Vector3(0, velocidadBala, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Si la y de la bala esta por encima se destruye
+        if (transform.position.y > 10.5)
+        {
+            Destroy(gameObject);
+            //print("destruida la bala");
+        }
+	}
+}
